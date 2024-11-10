bb178a806f0429195af43b78a2d3083e8d326da5
puppycat710 <puppycat710@proton.me>
2024-11-10 17:15:13 -0300

    hilltopads

---
+++ src/js/hilltopads.js
@@ -0,0 +1,11 @@
+;(function (hpqk) {
+	var d = document,
+		s = d.createElement('script'),
+		l = d.scripts[d.scripts.length - 1]
+	s.settings = hpqk || {}
+	s.src =
+		'//shocking-honey.com/c.Db9T6Qb_2/5IliSwWpQU9/NuTrcfyGNpT_gv0dMhyR0/1hNazMI/1DOBDJQg0A'
+	s.async = true
+	s.referrerPolicy = 'no-referrer-when-downgrade'
+	l.parentNode.insertBefore(s, l)
+})({})
